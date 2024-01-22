0aba7813006f6588c4b4593724eaec190e396125
Menelik Wubneh <mwubneh@z1r5p1.42lyon.fr>
2024-01-22 14:52:42 +0100

    adding angle vision

--- main.c
+++ main.c
@@ -6,14 +6,13 @@
 /*   By: edboutil <edboutil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/14 17:33:36 by edboutil          #+#    #+#             */
-/*   Updated: 2024/01/22 14:07:50 by mwubneh          ###   ########.fr       */
+/*   Updated: 2024/01/22 14:52:08 by mwubneh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
- #include "includes/cub3d.h"
-# include <X11/keysym.h>
-
-void color_pixels(t_mlx *mlx, int width, int height);
+# include "includes/cub3d.h"
+# define PI 3.14159265359
+void	color_pixels(t_mlx *mlx, int width, int height);
 void	draw_player(t_data data);
 
 void	print(t_mlx *mlx)
@@ -27,17 +26,24 @@ void	draw_player(t_data data)
 {
 	int	i;
 	int j;
+	int z;
 
 	i = 0;
-	while (i < 5)
+	z = 0;
+	while (i < 6)
 	{
 		j = 0;
-		while (j < 5)
+		while (j < 6)
 		{
 			data.addr[((int)data.player.pos_x + i) * WIN_WIDTH + ((int)data.player.pos_y + j)] = 0xFFD50000; // Rouge
 			++j;
 		}
 		++i;
+	}
+	while (z <= 10)
+	{
+		data.addr[((int)data.player.pos_x + i / 2 + (int)data.player.pdx * z) * WIN_WIDTH + ((int)data.player.pos_y + j / 2 + (int)data.player.pdy * z)] = 0xFFD50000; // Rouge
+		z++;
 	}
 }
 
@@ -57,13 +63,31 @@ int handle_key_press(int keycode, t_mlx *mlx)
 	if (keycode == XK_Escape)
 		close_window(mlx);
 	if (keycode == XK_w)
-		mlx->data.player.pos_x -= 10;
+	{
+		mlx->data.player.pos_x += mlx->data.player.pdx;
+		mlx->data.player.pos_y += mlx->data.player.pdy;
+	}
 	else if (keycode == XK_s)
-		mlx->data.player.pos_x += 10;
+	{
+		mlx->data.player.pos_x -= mlx->data.player.pdx;
+		mlx->data.player.pos_y -= mlx->data.player.pdy;
+	}
 	else if (keycode == XK_a)
-		mlx->data.player.pos_y -= 10;
+	{
+		mlx->data.player.pa += 0.1;
+		if (mlx->data.player.pa < 0)
+			mlx->data.player.pa += 2 * PI;
+		mlx->data.player.pdx = cos(mlx->data.player.pa) * 5;
+		mlx->data.player.pdy = sin (mlx->data.player.pa) * 5;
+	}
 	else if (keycode == XK_d)
-		mlx->data.player.pos_y += 10;
+	{
+		mlx->data.player.pa -= 0.1;
+		if (mlx->data.player.pa > 2 * PI)
+			mlx->data.player.pa -= 2 * PI;
+		mlx->data.player.pdx = cos(mlx->data.player.pa) * 5;
+		mlx->data.player.pdy = sin (mlx->data.player.pa) * 5;
+	}
 	print(mlx);
 	return (0);
 }
@@ -98,6 +122,10 @@ void	init_window(t_mlx	*mlx)
 		&mlx->data.endian);
 	mlx->data.player.pos_x = 300;
 	mlx->data.player.pos_y = 300;
+	mlx->data.player.pa = 0;
+	mlx->data.player.pdx = 0;
+	mlx->data.player.pdy = 0;
+
 
 }
 
