53693b341310f34388fdd0d28e804c92fa721e8d
Edgar Boutillot <edboutil@z1r1p2.42lyon.fr>
2024-03-29 13:19:12 +0100

    main norm

--- headers/cub3d.h
+++ headers/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: edboutil <edboutil@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 23:36:49 by mwubneh           #+#    #+#             */
-/*   Updated: 2024/03/29 12:46:57 by edboutil         ###   ########lyon.fr   */
+/*   Updated: 2024/03/29 13:08:01 by edboutil         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -151,14 +151,17 @@ void	map_cpy(char **map, char ***cpy);
 //-----------------------------mlx_init.c-----------------------------------//
 void	init_mlx(t_mlx *mlx, t_data *data);
 int		close_window(t_mlx *mlx);
-
 //-----------------------------mlx_utils.c-----------------------------------//
 void	ft_exit(t_data *data, t_mlx *mlx, int n, int c);
 int		close_window(t_mlx *mlx);
+//-----------------------------init_player.c-----------------------------------//
+void	ft_free_data(t_data *data, int i);
+double	get_start_angle(char c);
+void	init_player(t_mlx *mlx, t_data *data);
 
 void	color_pixels(t_mlx *mlx);
 void	raycast(t_player player, t_ray *selected);
 int		handle_key_press(int keycode, t_mlx *mlx);
 void	ft_free_data(t_data *data, int i);
-int	print_image(t_mlx *mlx);
+int		print_image(t_mlx *mlx);
 #endif
